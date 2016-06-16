2e72bfd03831480c7612798d37f1692cbc84c282
Daniel Pelayo <daniel.pelayo23@gmail.com>
2016-06-15 23:47:44 -0430

    funcionamiento basico del estacionamiento

--- sem_cli.c
+++ sem_cli.c
@@ -11,47 +11,91 @@
 #include <netdb.h> 
 #include <arpa/inet.h> 
 #include <sys/types.h>
+#include <stdio.h>
+#include <time.h>
 
 #define SERVER_PORT 4321 
 #define BUFFER_LEN 1024
 
 int main(int argc, char *argv[]) {
+
+/* Variables asociadas al uso de Sockets */
 char buf[BUFFER_LEN]; /* Buffer de recepción */
-int sockfd; /* descriptor a usar con el socket */
-struct sockaddr_in their_addr; /* almacenara la direccion IP y numero de puerto del servidor */
-struct hostent *he; /* para obtener nombre del host */
+char buf_copy[BUFFER_LEN]; /* Copia Buffer de recepción que se modificara para leer la respuesta*/
+int sockfd, portnum; /* descriptor a usar con el socket */
+struct sockaddr_in svr_addr; /* almacenara la direccion IP y numero de puerto del servidor */
+struct hostent * host_name; /* para obtener nombre del host */
 int addr_len, numbytes; /* conteo de bytes a escribir */
 
-if (argc != 3) {
-fprintf(stderr,"\nuso: %s cliente hostname mensaje\n", argv[0]);
-exit(1); }
+
+/*********************Verificacion de entrada*****************************/
+
+//Verificacion de numero de argumentos
+if (argc != 9) {
+	printf("Uso: %s -d <nombre_módulo_atención> -p <puerto_sem_svr> -c <op> -i <identificación_vehiculo>\n", argv[0]);
+	exit(1); 
+}
+//Verificacion de Banderas
+else if ( (strcmp(argv[1],"-d") != 0) || (strcmp(argv[3],"-p") != 0) || (strcmp(argv[5],"-c") != 0) || (strcmp(argv[7],"-i") != 0)   ){
+	fprintf(stderr,"Alguna bandera esta incorrecta\n");
+	fprintf(stderr,"Uso: %s -d <nombre_módulo_atención> -p <puerto_sem_svr> -c <op> -i <identificación_vehiculo>\n", argv[0]);
+	exit(1); 
+}
+//Verificacion del Puerto, (Num de cifras del puerto)
+else if( (strlen(argv[4]) != 5) ){
+    fprintf(stderr,"ERROR, el puerto no es compatible\n");
+    exit(1);
+}
+//Verificacion del Puerto, (Numeros de puertos validos: 20539, 20353, 20093)
+else if( (atoi(argv[4]) != 20539) && (atoi(argv[4]) != 20353)  && (atoi(argv[4]) != 20093) ){
+    fprintf(stderr,"ERROR, numero de puerto no valido\n");
+    exit(1);
+}
+else if( (strcmp(argv[6],"e") != 0) && (strcmp(argv[6],"s") != 0) ){
+    fprintf(stderr,"ERROR, Indicacion no valida:\n");
+    fprintf(stderr,"usar s para indicar salida de vehiculo\n");
+ 		fprintf(stderr,"usar e para indicar entrada de vehiculo\n");
+    exit(1);
+}
+/**************************Fin revision entrada*************************/
 
 /* convertimos el hostname a su direccion IP */ 
-if ((he=gethostbyname(argv[1])) == NULL) {
+if ((host_name=gethostbyname(argv[2])) == NULL) {
 perror("gethostbyname");
 exit(1); }
 
+portnum = atoi(argv[4]);
 /* Creamos el socket */
 if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
 perror("socket");
 exit(2); }
-
 /* a donde mandar */
-their_addr.sin_family = AF_INET; /* usa host byte order */ 
-their_addr.sin_port = htons(SERVER_PORT); /* usa network byte order */ 
-their_addr.sin_addr = *((struct in_addr *)he->h_addr); bzero(&(their_addr.sin_zero), 8); /* pone en cero el resto */
+svr_addr.sin_family = AF_INET; /* usa host byte order */ 
+svr_addr.sin_port = htons(portnum); /* usa network byte order */ 
+svr_addr.sin_addr = *((struct in_addr *)host_name->h_addr); bzero(&(svr_addr.sin_zero), 8); /* pone en cero el resto */
 
-/* enviamos el mensaje */
-if ((numbytes=sendto(sockfd,argv[2],strlen(argv[2]),0,(struct sockaddr *)&their_addr, sizeof(struct sockaddr))) == -1) {
+
+/* enviamos info al servidor en el buffer*/
+memset(buf,'\0', BUFFER_LEN);//Se borra el buffer por si acaso habia algo antes q' fastidie
+strcat(buf,(argv[6]));// Si el vehiculo entrara o saldra
+strcat(buf," ");//separador
+strcat(buf,(argv[8]));// Id del vehiculo
+strcat(buf,"\n");//fin del buffer
+
+if ((numbytes = sendto(sockfd,buf,strlen(buf),0,(struct sockaddr *)&svr_addr, sizeof(struct sockaddr))) == -1) {
 perror("sendto");
 exit(2); }
 
-if ((numbytes=recvfrom(sockfd, buf, BUFFER_LEN, 0, (struct sockaddr *)&their_addr,
-(socklen_t *)&addr_len)) == -1) { 
-perror("recvfrom");
-exit(3);}
+memset(buf,'\0', BUFFER_LEN);//Se borra el buffer por si acaso habia algo antes q' fastidie
 
-printf("enviados %d bytes hacia %s\n",numbytes,inet_ntoa(their_addr.sin_addr));
+/* recibimos respuesta del servidor*/
+if ((numbytes = recvfrom(sockfd, buf, BUFFER_LEN, 0, (struct sockaddr *)&svr_addr,(socklen_t *)&addr_len)) == -1) { 
+perror("recvfrom");
+exit(2);}
+
+printf("enviados %d bytes hacia %s\n",numbytes,inet_ntoa(svr_addr.sin_addr));
+printf("Respuesta del servidor: %s\n", buf);
+
 /* cierro socket */ 
 close(sockfd); 
 exit (0);
--- sem_svr.c
+++ sem_svr.c
@@ -11,18 +11,37 @@
 #include <netdb.h> 
 #include <arpa/inet.h> 
 #include <sys/types.h>
+#include <stdio.h>
+#include <time.h>
 
-#define SERVER_PORT 4321 
-#define BUFFER_LEN 1024
+#define SERVER_PORT 20539 //Puerto a usar, esto habra q cambiarlo
+#define BUFFER_LEN 1024 //longitud del buffer de lectura
+int CAP_EST = 10;//Capacidad del estacionamiento
+int ids_vehiculos[200];
+
 
 int main(int argc, char *argv[]) {
 
 int sockfd; /* descriptor para el socket */
 struct sockaddr_in my_addr; /* direccion IP y numero de puerto local */
 struct sockaddr_in their_addr; /* direccion IP y numero de puerto del cliente */
-/* addr_len contendra el taman~o de la estructura sockadd_in y numbytes el * numero de bytes recibidos */
-int addr_len, numbytes;
+int addr_len, numbytes;/* addr_len contendra el tamano de la estructura sockadd_in y numbytes el * numero de bytes recibidos */
 char buf[BUFFER_LEN]; /* Buffer de recepción */
+char buf_copy[BUFFER_LEN]; /* Copia Buffer de recepción que se modificara para leer la respuesta*/
+char op[1];
+char id[20];
+char idTicket[4];
+time_t tiempo;
+struct tm *tlocal;
+char FechaHora[128];
+//Inicializacion de arreglo de ids de tickets de vehiculos
+int i;
+for (i = 0; i < 200; ++i)
+{
+	ids_vehiculos[i] = i+1;
+}
+
+/////////////////////////////////////////////////////////
 /* se crea el socket */
 if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
 perror("socket");
@@ -37,9 +56,14 @@ printf("Asignado direccion al socket ....\n");
 if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1) {
 perror("bind");
 exit(2); }
-/* Se reciben los datos (directamente, UDP no necesita conexión) */
+///////////////////////////////////////////////////////////////
+
+//Servidor se queda activo esperando que alguien se comunique con el
+while(1){
+	/* Se reciben los datos (directamente, UDP no necesita conexión) */
 addr_len = sizeof(struct sockaddr);
 printf("Esperando datos ....\n");
+memset(buf,'\0', BUFFER_LEN);//Se borra el buffer por si acaso habia algo antes q' fastidie
 
 if ((numbytes=recvfrom(sockfd, buf, BUFFER_LEN, 0, (struct sockaddr *)&their_addr,
 (socklen_t *)&addr_len)) == -1) { perror("recvfrom");
@@ -47,14 +71,56 @@ exit(3); }
 /* Se visualiza lo recibido */
 
 printf("paquete proveniente de : %s\n",inet_ntoa(their_addr.sin_addr)); 
-printf("longitud del paquete en bytes: %d\n",numbytes); 
-buf[numbytes] = '\0';
+printf("longitud del paquete en bytes: %d\n",numbytes);
 printf("el paquete contiene: %s\n", buf);
+strcpy(buf_copy, buf);
+strcpy(op, strtok(buf_copy, " "));//op contiene el caracter que nos dice 
+strcpy(id, strtok(buf_copy, "\n"));
+
 
 /* enviamos respuesta al cliente */
-if ((numbytes=sendto(sockfd,"exito\n",strlen(argv[2]),0,(struct sockaddr *)&their_addr, sizeof(struct sockaddr))) == -1) {
+memset(buf,'\0', BUFFER_LEN);//Se borra el buffer por si acaso habia algo antes q' fastidie
+
+if (strcmp(op,"e") == 0)
+{
+	//Verificamos si hay puestos
+	if (CAP_EST > 0)
+	{
+
+	  tiempo = time(0);
+	  tlocal = localtime(&tiempo);	  
+	  strftime(FechaHora,128,"%d/%m/%y %H:%M:%S",tlocal);//FechaHora contiene String con la fecha y hora actual local
+	  i = 0;
+	  //positivos indican que el id no ha sido asignado, negativo, ya fue asignado
+	  while(ids_vehiculos[i] < 0){ //Consigue el primer id que no ha sido asignado, esta en la pos i
+	  	i++;
+	  }
+	  sprintf(idTicket, "%d", ids_vehiculos[i]);
+	  ids_vehiculos[i] = (i+1)*-1;
+	  CAP_EST--;
+
+	  //Se carga mensaje a buffer:
+		strcat(buf,FechaHora);// Si el vehiculo entrara o saldra
+		strcat(buf," ");//separador
+		strcat(buf,idTicket);// Id del vehiculo
+		strcat(buf,"\n");//fin del buffer	  		
+
+	}
+	else{
+		strcat(buf,"leveee, el est esta full compai");
+	}
+}
+else{// hay q Verificar si alguien quiere salir pero hay full cap
+	CAP_EST++;
+	strcat(buf,"Hasta luego");
+}
+
+if ((numbytes=sendto(sockfd,buf,strlen(buf),0,(struct sockaddr *)&their_addr, sizeof(struct sockaddr))) == -1) {
 perror("sendto");
 exit(2); }
+
+}
+
 
 /* cerramos descriptor del socket */ 
 close(sockfd);
